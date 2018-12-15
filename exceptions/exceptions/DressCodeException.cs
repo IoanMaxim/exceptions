a351286e19cbf27fafd1dbe1bdb21e0314e3ff14
IoanMaxim <ioan.maxim2016@gmail.com>
2018-12-15 12:16:50 +0200

    Added costume exception

---
+++ exceptions/exceptions/DressCodeException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace exceptions
+{
+    class DressCodeException:ApplicationException
+    {
+        string message = "You are not wearing an Elf costume and u are underage.";
+        public DressCodeException()//constructor
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
--- exceptions/exceptions/Program.cs
+++ exceptions/exceptions/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
-
+//EXCEPTIONS:
+//
 namespace exceptions
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //try
+            //{
+            //    int a = 100;
+            //    int b = a / 0;
+            //    Console.WriteLine(a);
+            //}
+            //catch (DivideByZeroException exception)//exception este un nume de variabila;
+            //{
+            //    Console.WriteLine("Ups, something is wrong.");
+            //    Console.WriteLine(exception.Message);
+            //    Console.WriteLine(exception.StackTrace);
+            //    Console.WriteLine(exception.Source);
+            //}
+
+            try
+            {
+                Person ionut = new Person(15);
+                Person ionut1 = new Person(-1);
+                Person ionut2 = new Person(2);
+                Person ionut3 = new Person(81);
+            }
+            catch (ArgumentException e)
+            {
+                
+                //Console.WriteLine(e);//This will show all the error;
+                Console.WriteLine(e.Message + e.GetType());
+                //throw;
+            }
+            catch (DressCodeException ex)
+            {
+                Console.WriteLine(ex.Message + ex.GetType());
+            }
+            finally
+            {
+                //close DataBase conection;
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             Console.ReadLine();
         }
     }
